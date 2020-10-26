17b4fcec198fde567137797aadef64e45cd7dbe8
Asiia Pakhomova <askapakhomova@gmail.com>
2020-10-27 01:03:20 +0300

    Вынесены в константы данные

--- hw_task4/scr/TariffHours.php
+++ hw_task4/scr/TariffHours.php
@@ -1,12 +1,13 @@
 <?php
 class TariffHours extends TariffAbstract
 {
-    const PRICE_MIN =  200 / 60;
+    const PRICE_MIN =  200/60;
     const PRICE_DIS = 0;
+    const MIN_IN_HOUR = 60;
 
-    public function __construct(int $distance, int $min)
-    {
-        parent::__construct($distance, $min);
-        $this->min = $this->min - $this->min % 60 + 60;
-    }
+  public function __construct($distance, $min)
+  {
+      parent::__construct($distance, $min);
+      $this->min = $this->min - $this->min % $this::MIN_IN_HOUR + $this::MIN_IN_HOUR;
+  }
 }
